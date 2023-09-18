c56d51c38d37e0582bd5d0e3cf29a69f3104d02f
NguyenNam1102 <namnguyenlllh@gmail.com>
2023-09-18 23:01:44 +0700

    create nav and ejs loop data

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -9,7 +9,7 @@ let getHomePage = (req, res) => {
             console.log('>>> CHECK: ');
             console.log(results);
             data = results.map((row) => { return row })
-            return res.render('index.ejs', { dataUser: JSON.stringify(data) })
+            return res.render('index.ejs', { dataUser: data })
         }
     );
 }
